b6ba8b906ce577bf136fcbdf5c936d3f9d4279be
Jan-Marco Haldemann <146882261+janmacro@users.noreply.github.com>
2024-06-17 17:56:36 +0200

    chore: shorten config

--- plantclef-pretrained.py
+++ plantclef-pretrained.py
@@ -108,13 +108,10 @@ optim_wrapper = dict(
 
 param_scheduler = dict(
     type='CosineAnnealingLR',
-    T_max=10,
     by_epoch=True,
-    begin=0,
-    end=10
 )
 
-train_cfg = dict(by_epoch=True, max_epochs=10, val_interval=1)
+train_cfg = dict(by_epoch=True, max_epochs=20, val_interval=1)
 val_cfg = dict()
 test_cfg = dict()
 
