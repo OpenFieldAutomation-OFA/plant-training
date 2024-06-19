6b7a7e768a555492e99674e89bcb3cf48fc25dd1
Jan-Marco Haldemann <146882261+janmacro@users.noreply.github.com>
2024-06-19 19:45:50 +0200

    feat: remove pretrained head

--- create_models.py
+++ create_models.py
@@ -2,13 +2,14 @@ import argparse
 import timm
 import torch
 import os
+import torch.nn as nn
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--plantclef_model',
-                    default="/mnt/data/plantclef/pretrained/model_best.pth.tar",
+                    default="model_best.pth.tar",
                     help="Path to pretrained PlantCLEF model.")
 parser.add_argument('--model_dir',
-                    default="/mnt/data/plantclef/pretrained",
+                    default="/mnt/data/plantclef2/pretrained",
                     help="Directory where models are saved.")
 args = parser.parse_args()
 
@@ -16,9 +17,11 @@ plantclef_model = timm.create_model('vit_base_patch14_reg4_dinov2.lvd142m',
                                     pretrained=False, num_classes=7806,
                                     checkpoint_path=args.plantclef_model)
 
-dinov2_model = timm.create_model('vit_base_patch14_reg4_dinov2',
-                                 pretrained=True, num_classes=7806)
+empty_head = nn.Linear(plantclef_model.embed_dim, 7806, bias=True)
+head_state_dict = empty_head.state_dict()
+state_dict = plantclef_model.state_dict()
+state_dict['head.weight'] = head_state_dict['weight']
+state_dict['head.bias'] = head_state_dict['bias']
 
-torch.save(plantclef_model.state_dict(), os.path.join(args.model_dir, 'plantclef.pth'))
-torch.save(dinov2_model.state_dict(), os.path.join(args.model_dir, 'dinov2.pth'))
+torch.save(state_dict, os.path.join(args.model_dir, 'plantclef_empty_head.pth'))
 
