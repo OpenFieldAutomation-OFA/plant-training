fe33082de7286f4a147accf8f02898e13bcd64d4
Jan-Marco Haldemann <146882261+janmacro@users.noreply.github.com>
2024-07-08 23:48:32 +0200

    feat: topk binary metric

--- custom_metric/binary.py
+++ custom_metric/binary.py
@@ -11,23 +11,22 @@ from mmpretrain.registry import METRICS
 
 @METRICS.register_module()
 class BinaryMetric(BaseMetric):
-    def __init__(self, class_id: int, thrs: float = 0) -> None:
+    def __init__(self, class_id: int, thrs: float = 0, topk=1) -> None:
         super().__init__()
         self.class_id = class_id
         self.thrs = thrs
+        self.topk = topk
 
     def process(self, data_batch, data_samples: Sequence[dict]) -> None:
         for data_sample in data_samples:
             if 'pred_score' in data_sample:
-                pred_score = data_sample['pred_score']
-                pred_label = pred_score.argmax(dim=0, keepdim=True)
-                if pred_score[pred_label] < self.thrs:
-                    pred_label = None
-            else:
-                pred_label = data_sample['pred_label']
+                pred_score = data_sample['pred_score'].cpu()
+                # pred_label = pred_score.argmax(dim=0, keepdim=True)
+                # if pred_score[pred_label] < self.thrs:
+                #     pred_label = None
 
             self.results.append({
-                'pred_label': pred_label,
+                'pred_score': pred_score,
                 'gt_label': data_sample['gt_label'],
             })
 
@@ -36,15 +35,26 @@ class BinaryMetric(BaseMetric):
         tn = 0
         fp = 0
         fn = 0
+        fns = []
+        fps = []
         for result in results:
-            if result['pred_label'] == self.class_id and result['gt_label'] == self.class_id:
+            pred_scores = result['pred_score']
+            topk_pred_scores, topk_classes = torch.topk(pred_scores, self.topk, dim=0)
+            predicted = False
+            if self.class_id in topk_classes:
+                if pred_scores[self.class_id] >= self.thrs:
+                    predicted = True
+
+            if predicted and result['gt_label'] == self.class_id:
                 tp += 1
-            if result['pred_label'] != self.class_id and result['gt_label'] != self.class_id:
+            if not predicted and result['gt_label'] != self.class_id:
                 tn += 1
-            if result['pred_label'] == self.class_id and result['gt_label'] != self.class_id:
+            if predicted and result['gt_label'] != self.class_id:
                 fp += 1
-            if result['pred_label'] != self.class_id and result['gt_label'] == self.class_id:
+                fps.append(result['pred_label'])
+            if not predicted and result['gt_label'] == self.class_id:
                 fn += 1
+                fns.append(result['pred_label'])
             
         acc = (tp + tn) / (tp + tn + fp + fn)
         if tp + fp == 0:
@@ -63,4 +73,4 @@ class BinaryMetric(BaseMetric):
             f1 = None
         
         return {'accuracy': acc, 'precision': prec, 'recall': rec, 'f1': f1,
-                'tp': tp, 'tn': tn, 'fp': fp, 'fn': fn}
+                'tp': tp, 'tn': tn, 'fp': fp, 'fn': fn, 'fns': fns, 'fps': fps}
