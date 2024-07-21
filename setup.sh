f0b0608c8f08756e8bdd8c22e41f3222bd3a502f
Jan-Marco Haldemann <146882261+janmacro@users.noreply.github.com>
2024-07-21 14:56:25 +0000

    feat: finetune config

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
+apt update
 apt install python3-venv -y
 python3 -m venv .venv
 source .venv/bin/activate
-pip install torch torchvision --index-url https://download.pytorch.org/whl/cu121
+pip install torch torchvision
 git clone https://github.com/open-mmlab/mmpretrain.git
 cp custom_metric/* mmpretrain/mmpretrain/evaluation/metrics
 cd mmpretrain
